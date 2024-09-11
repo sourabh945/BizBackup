c638a444e8b553942e72844ab5240c8f2a1aa078
sourabh945 <sourabhsheokand945@gmail.com>
2024-09-11 14:13:27 +0530

    deleted:    data.json 	deleted:    data2.json 	renamed:    New/main.py -> main.py 	renamed:    New/modules/configurator.py -> modules/configurator.py 	renamed:    New/modules/error.py -> modules/error.py 	renamed:    New/modules/f_ops.py -> modules/f_ops.py 	renamed:    New/modules/ui.py -> modules/ui.py 	renamed:    New/modules/utils.py -> modules/utils.py 	renamed:    New/requirement.txt -> requirement.txt 	deleted:    windows version/main.py 	deleted:    windows version/modules/configurator.py 	deleted:    windows version/modules/error.py 	deleted:    windows version/modules/file_operation.py 	deleted:    windows version/modules/test/utils_test.py 	deleted:    windows version/modules/ui.py 	deleted:    windows version/modules/utils.py

--- windows version/modules/test/utils_test.py
+++
@@ -1,9 +0,0 @@
-from utils import run_cmd
-
-print(run_cmd('dir',shell=True).stdout)
-
-print(run_cmd('rclone version').returncode)
-
-print(run_cmd('ls',shell=True).stderr)
-
-print(run_cmd('rclone listremotes',inputs=['sourabh']))
